904ec80694f19612abf26cac756d8be9aca11319
MannyWeeb <59584679+MannyWeeb@users.noreply.github.com>
2026-08-24 11:01:31 +0800

    bruv

--- start.sh
+++ start.sh
@@ -2,18 +2,13 @@
 cd apps/backend
 
 echo "Running database migrations..."
-#pnpm medusa db:migrate
+pnpm medusa db:migrate
 
 echo "Seeding database..."
 cd ../../
+
 pnpm backend:seed || echo "Seeding failed, continuing..."
 
-cd apps/backend
-
-pnpm install
-
-echo "Migrating bullshit"
-pnpm medusa db:migrate
 
 echo "Starting Medusa server..."
 pnpm start
